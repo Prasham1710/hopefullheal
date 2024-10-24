0c0c5e0df43607ab55efa5b3dcebf077aa517b9a
Prasham1710 <jainprasham17@gmail.com>
2024-10-24 16:47:45 +0530

    cards

--- app/(consult)/appointment/(pages)/doctors/page.tsx
+++ app/(consult)/appointment/(pages)/doctors/page.tsx
@@ -2,9 +2,10 @@ import ProductCard from "@/app/(consult)/appointment/_components/ProductCard/pag
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-teal-300 via-teal-400 to-teal-500">
-      <ProductCard />
+    <div className="w-full min-h-screen bg-gradient-to-r from-teal-300 via-teal-400 to-teal-500 overflow-auto">
+      <div className="container mx-auto py-10">
+        <ProductCard />
+      </div>
     </div>
   );
 }
-
